9fd289fe210958ba126bef14acd3d2b3ec1c78eb
lzr247 <lazarconic@yahoo.com>
2021-04-29 09:48:06 +0200

    bem methodology on home page + some changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <v-app>
     <div id="app">
-      <!-- <transition name="router-anim" enter-active-class="animated animate__backInLeft" leave-active-class="animated animate__backOutDown">
-        <router-view/>
-      </transition> -->
       <router-view></router-view>
     </div>
   </v-app>
 </template>
 
 <style lang="scss">
-// @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
-#app {
-  font-family: 'Cabin', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  text-decoration: none;
-
-}
-//  .page {
-//     position: absolute;
-//     width: inherit;
-//   }
+  #app {
+    font-family: 'Cabin', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    text-decoration: none;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="home page">
-    <div class="left">
-      <div class="content">
-        <h1>MikiPh</h1>
-        
+  <div class="home">
+
+    <div class="home__contentLeft">
+      <div class="home__contentLeftMain">
+        <h2>MikiPh</h2>
         <span>
           <h1>Youtube // coffee addict</h1>
           <p>Photographer & Filmmaker. Lover of coffee and good stories. Currently residing in Smederevo, Serbia running big channel on YouTube; MNB Anime. </p>
@@ -11,23 +11,22 @@
           <v-btn to="/about">About me</v-btn>
           <v-btn to="/contact">Contact</v-btn>
         </span>
-
-        <span class="bottom-span" style="display:block;">
-          <v-btn outlined href="https://www.facebook.com/milorad.simic.509" target="blank"><i class="fab fa-facebook-f"></i></v-btn>
-          <v-btn outlined href="https://www.instagram.com/miki_ph_/" target="blank"><i class="fab fa-instagram"></i></v-btn>
-          <v-btn outlined href="https://www.youtube.com/channel/UCI48hhOD7fGAPKlVIyE-Zbw" target="blank"><i class="fab fa-youtube"></i></v-btn>
+        <span class="home__spanBottom">
+          <v-btn href="https://www.facebook.com/milorad.simic.509" target="blank"><i class="fab fa-facebook-f"></i></v-btn>
+          <v-btn href="https://www.instagram.com/miki_ph_/" target="blank"><i class="fab fa-instagram"></i></v-btn>
+          <v-btn href="https://www.youtube.com/channel/UCI48hhOD7fGAPKlVIyE-Zbw" target="blank"><i class="fab fa-youtube"></i></v-btn>
         </span>
-
-
       </div>
     </div>
-    <div class="right">
+
+    <div class="home__contentRight">
       <v-img
           lazy-src="@/assets/miki1.jpg"
           src="@/assets/miki1.jpg"
           style="height:100vh;"
         ></v-img>
     </div>
+
   </div>
 </template>
 
@@ -38,115 +37,112 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  .home{
-    overflow: hidden;
-    color: white;
-  }
-  .left{
-    width:50%;
-    height: 100vh;
-    display: block;
-    float:left;
-    background-color: #93495E;
-  }
-  .right{
-    width: 50%;
-    display: block;
-    float:right;
-  }
-  .left > .content{
-    height:100vh;
-    display:flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-  }
-  //naslov
-  .content > h1:first-child{
-    font-family:'Yellowtail', cursive!important;
-    font-size: 4em;
-    padding-top:10px;
-  }
-  //centralni naslov
-  #app > div > div.left > div > span:nth-child(2) > h1{
-    font-size: 5em;
 
-  }
-  #app > div > div.left > div > span:nth-child(2) > p{
-    font-size: 17px;
-    padding: 20px 60px;
-  }
-  .left > div > span > a{
-    color:white;
-    font-family: 'Andika New Basic', sans-serif;
-    background-color:#93495E!important;
-    border: 2px solid #fff;
-    margin: 0 10px;
-    font-weight: bold;
-    border-radius: 0;
-  }
-  #app > div > div.left > div > span:nth-child(2) > a > span{
-    letter-spacing: 0.2px;
-    padding: 80px 30px!important;
-  }
-  .left > div > span > a:hover{
-    color: #93495E!important;
-    background-color:#fff!important;
-    border: 2px solid #93495E!important;
-    transition: all 0.3s!important;
-  }
-  //donja navigacija
-  #app > div > div.left > div > span.bottom-span{
-    margin-bottom: 30px;
-  }
-  #app > div > div.left > div > span.bottom-span:hover button{
-    color: rgb(129, 129, 129);
-  }
-  #app > div > div.left > div > span.bottom-span > button{
-    background-color: inherit;
+  .home {
+    // overflow: hidden;
     color: white;
-    margin-right: 0.7rem;
-  }
-  #app > div > div.left > div > span.bottom-span > button:hover{
-    color: white;
-  }
-  #app > div > div.left > div > span.bottom-span > button > span > i{
-    font-size: 25px;
+    &__contentLeft {
+      width: 50%;
+      height: 100vh;
+      display: block;
+      float: left;
+      background-color: #93495e;
+    }
+    &__contentLeftMain {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        h2 {
+          font-family: 'Yellowtail', cursive!important;
+          font-size: 4em;
+          padding-top: 10px;
+        }
+        span {
+          h1 {
+            font-size: 5em;
+          }
+          p {
+            font-size: 17px;
+            padding: 20px 60px;
+          }
+          a {
+            color: white;
+            font-family: 'Andika New Basic', sans-serif;
+            background-color: #93495e!important;
+            border: 2px solid #fff;
+            margin: 10px 10px 0 10px;
+            font-weight: bold;
+            border-radius: 0;
+            &:hover {
+              color: #93495e!important;
+              background-color: #fff!important;
+              border: 2px solid #fff;
+              transition: all .2s;
+            }
+          }
+        }
+    }
+    &__spanBottom{
+      display: block;
+      margin-bottom: 30px;
+      &:hover a{
+        color: rgb(129, 129, 129)!important;
+        border-color: rgb(129, 129, 129)!important;
+      }
+      a {
+        background-color: inherit;
+        color: white;
+        margin-right: .7rem;
+        transition: all .2s;
+        &:hover {
+          color: white!important;
+          border-color: #fff!important;
+        }
+        i {
+          font-size: 25px;
+        }
+      }
+    }
+    &__contentRight {
+      width: 50%;
+      display: block;
+      float: right;
+    }
   }
 
-
-  @media screen and(max-width:768px){
-    .home{
+  @media screen and (max-width: 768px) {
+    .home {
       display: flex;
       flex-direction: column;
       overflow: visible;
-    }
-    .left{
-      width:100%;
-      height: 100%;
-      order:3;
-    }
-    .right{
-      width:100%;
-      height: 50vh;
-      order:1;
-    }
-    .left > .content{
-      height: 100%;
-    }
-    #app > div > div.right > div{
-      height: 50vh!important;
-    }
-    //donji kontakt
-    #app > div > div.left > div > span.bottom-span{
-      margin-top: 10px;
+      &__contentLeft {
+        width: 100%;
+        height: 100%;
+        order: 2;
+      }
+      &__contentLeftMain {
+        height: 100%;
+      }
+      &__spanBottom {
+        margin-top: 10px;
+      }
+      &__contentRight {
+        width: 100%;
+        height: 50vh;
+        order: 1;
+        div {
+          height: 50vh!important;
+        }
+      }
     }
   }
-  @media screen and(max-width:423px){
-    //kontakt dugme
-    #app > div > div.left > div > span:nth-child(2) > a:nth-child(5){
-      margin-top:10px;
-    }
 
-  }
+  // @media screen and (max-width: 423px) {
+  //   .home__contentLeftMain > span:nth-child(2) > a:nth-child(5) {
+  //     margin-top: 10px;
+  //   }
+  // }
+  
 </style>
